951cc822c9d4bb48f60cbb57f794e563a634fdde
Freddy Remache <fremache3525@uta.edu.ec>
2023-03-23 09:58:45 -0500

    commit 9:58

---
+++ SegundaWebApplication/Models/Reply.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SegundaWebApplication.Models
+{
+    public class Reply
+    {
+   
+            public int result { get; set; }
+            public object token { get; set; }
+            public string message { get; set; }
+        
+    }
+}
